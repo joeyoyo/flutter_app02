14d5e2a91dbfd5b2d56f4cfb7ef6be04bc6412d8
joeyoyo <l>
2020-11-27 15:52:52 +0800

    随便提交

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,71 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'showModalBottomSheet'),
+      home: new MyHomePage(title: '采用手势缩放图片'),
+    );
+  }
+}
+
+class SlideTransitionX extends AnimatedWidget {
+  SlideTransitionX({
+    Key key,
+    @required Animation<double> position,
+    this.transformHitTests = true,
+    this.direction = AxisDirection.down,
+    this.child,
+  })  : assert(position != null),
+        super(key: key, listenable: position) {
+    // 偏移在内部处理
+    switch (direction) {
+      case AxisDirection.up:
+        _tween = Tween(begin: Offset(0, 1), end: Offset(0, 0));
+        break;
+      case AxisDirection.right:
+        _tween = Tween(begin: Offset(-1, 0), end: Offset(0, 0));
+        break;
+      case AxisDirection.down:
+        _tween = Tween(begin: Offset(0, -1), end: Offset(0, 0));
+        break;
+      case AxisDirection.left:
+        _tween = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+        break;
+    }
+  }
+
+  Animation<double> get position => listenable;
+
+  final bool transformHitTests;
+
+  final Widget child;
+
+  //退场（出）方向
+  final AxisDirection direction;
+
+  Tween<Offset> _tween;
+
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = _tween.evaluate(position);
+    if (position.status == AnimationStatus.reverse) {
+      switch (direction) {
+        case AxisDirection.up:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.right:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+        case AxisDirection.down:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.left:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+      }
+    }
+    return FractionalTranslation(
+      translation: offset,
+      transformHitTests: transformHitTests,
+      child: child,
     );
   }
 }
@@ -28,40 +92,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _count = 0;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text("对话框2"),
-            onPressed: () async {
-              int type = await _showModalBottomSheet();
-              print(type);
-            },
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                //执行缩放动画
+                return SlideTransitionX(
+                  child: child,
+                  direction: AxisDirection.right, //上入下出
+                  position: animation,
+                );
+              },
+              child: Text(
+                '$_count',
+                //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
+                key: ValueKey<int>(_count),
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+            RaisedButton(
+              child: const Text(
+                '+1',
+              ),
+              onPressed: () {
+                setState(() {
+                  _count += 1;
+                });
+              },
+            ),
+          ],
+        ),
       ),
-    );
-  }
-
-  Future<int> _showModalBottomSheet() {
-    return showModalBottomSheet<int>(
-      context: context,
-      builder: (BuildContext context) {
-        return ListView.builder(
-          itemCount: 30,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text("$index"),
-              onTap: () => Navigator.of(context).pop(index),
-            );
-          },
-        );
-      },
     );
   }
 }
